9c2a433a1a1b4470ca570d6c9610d230ab69d62d
Vladyslav Anokhin <vladyslav.anokhin@sigma.software>
2020-01-23 19:33:28 +0200

    Added eg027 example

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const express = require('express')
     , eg024 = require('./lib/examples/eg024CreateBrand')
     , eg025 = require('./lib/examples/eg025ApplyBrandToEnvelope')
     , eg026 = require('./lib/examples/eg026ApplyBrandToTemplate')
+    , eg027 = require('./lib/examples/eg027PermissionChangeSingleSetting')
     , eg028 = require('./lib/examples/eg028CreatePermission')
     ;
 
@@ -141,6 +142,8 @@ let app = express()
   .post('/eg025', eg025.createController)
   .get('/eg026', eg026.getController)
   .post('/eg026', eg026.createController)
+  .get('/eg027', eg027.getController)
+  .post('/eg027', eg027.createController)
   .get('/eg028', eg028.getController)
   .post('/eg028', eg028.createController)
   ;
