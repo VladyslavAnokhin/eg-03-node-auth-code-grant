4cc55c8681ad80c8bf722c35df6527b93defa903
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2020-01-28 16:17:01 +0200

    Add 029

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ const express = require('express')
     , eg025 = require('./lib/examples/eg025ApplyBrandToEnvelope')
     , eg026 = require('./lib/examples/eg026ApplyBrandToTemplate')
     , eg028 = require('./lib/examples/eg028CreatePermission')
+    , eg029 = require('./lib/examples/eg029DeletePermission')
     ;
 
 const PORT = process.env.PORT || 5000
@@ -143,6 +144,8 @@ let app = express()
   .post('/eg026', eg026.createController)
   .get('/eg028', eg028.getController)
   .post('/eg028', eg028.createController)
+  .get('/eg029', eg029.getController)
+  .post('/eg029', eg029.createController)
   ;
 
 function dsLoginCB1 (req, res, next) {req.dsAuthCodeGrant.oauth_callback1(req, res, next)}
